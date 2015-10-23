4561a030067e865bb5d3d92d7b16a40bd968a1f0
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2015-10-23 12:25:13 +0100

    separating reporter container

---
+++ example/bin/test_commons.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+green="\033[32m"
+red="\033[31m"
+reset="\033[0m"
+
+assert_eq() {
+  if [ "$2" == "$3" ]
+  then
+    echo -e "  ${green}$1: $2 == $3${reset}"
+  else
+    echo -e "  ${red}$1: $2 <> $3${reset}"
+    exit -1
+  fi
+}
+
+assert_lt() {
+  if [ "$2" -lt "$3" ]
+  then
+    echo -e "  ${green}$1: $2 < $3${reset}"
+  else
+    echo -e "  ${red}$1: $2 >= $3${reset}"
+    exit -1
+  fi
+}
+
+assert_gt() {
+  if [ "$2" -gt "$3" ]
+  then
+    echo -e "  ${green}$1: $2 > $3${reset}"
+  else
+    echo -e "  ${red}$1: $2 <= $3${reset}"
+    exit -1
+  fi
+}
+
+assert_le() {
+  if [ "$2" -le "$3" ]
+  then
+    echo -e "  ${green}$1: $2 <= $3${reset}"
+  else
+    echo -e "  ${red}$1: $2 > $3${reset}"
+    exit -1
+  fi
+}
+
+assert_ge() {
+  if [ "$2" -ge "$3" ]
+  then
+    echo -e "  ${green}$1: $2 >= $3${reset}"
+  else
+    echo -e "  ${red}$1: $2 < $3${reset}"
+    exit -1
+  fi
+}

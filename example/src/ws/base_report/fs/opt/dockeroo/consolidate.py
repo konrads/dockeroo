fb77c62cf2690e1baca8b895ea5d9c3745ff66fa
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2015-10-29 10:39:03 +0000

    changing python scripts for reusability

--- example/src/ws/base_report/fs/opt/dockeroo/consolidate.py
+++ example/src/ws/base_report/fs/opt/dockeroo/consolidate.py
@@ -12,6 +12,8 @@ import math
 import sys
 
 def combine_status(start_status, end_status):
+    """Combines start audit status with end|error audit status.
+    """
     if start_status == 'start' and end_status == 'end':
         return 'success'
     elif start_status == 'start' and type(end_status) == float and math.isnan(end_status):
@@ -21,19 +23,28 @@ def combine_status(start_status, end_status):
     else:
         return 'not_started'
 
-parser = argparse.ArgumentParser(description='Merges starts and ends/errors.')
-parser.add_argument('data_file', help='data csv file, with header')
-args = parser.parse_args()
+def consolidate(df):
+    """Produce an outer join from started and non-started portions of DataFrame,
+    calculating delta and consolidating status.
+    """
+    [status_col, cat_col, id_col, ts_col] = df.columns
+    
+    started_df = df[df[status_col] == 'start']
+    not_started_df = df[df[status_col] != 'start']
+    consolidated_df = pd.merge(started_df, not_started_df, how='outer', on=[cat_col, id_col], suffixes=['', '_end'])
+    consolidated_df['delta'] = consolidated_df[ts_col+'_end'] - consolidated_df[ts_col]
+    consolidated_df[status_col] = consolidated_df[status_col].combine(consolidated_df[status_col+'_end'], combine_status)
+    del consolidated_df[status_col+'_end']
+    del consolidated_df[ts_col+'_end']
+    return consolidated_df
 
-init_df = pd.read_csv(args.data_file, na_values=['na'])
-[status_col, cat_col, id_col, ts_col] = init_df.columns
+def main():
+    parser = argparse.ArgumentParser(description='Consolidate start|end|error audits.')
+    parser.add_argument('data_file', help='data csv file, with header')
+    args = parser.parse_args()
+    
+    df = pd.read_csv(args.data_file, na_values=['na'])
+    print consolidate(df).to_csv(index=False, float_format='%.0f')
 
-started_df = init_df[init_df[status_col] == 'start']
-not_started_df = init_df[init_df[status_col] != 'start']
-consolidated_df = pd.merge(started_df, not_started_df, how='outer', on=[cat_col, id_col], suffixes=['', '_end'])
-consolidated_df['delta'] = consolidated_df[ts_col+'_end'] - consolidated_df[ts_col]
-consolidated_df[status_col] = consolidated_df[status_col].combine(consolidated_df[status_col+'_end'], combine_status)
-del consolidated_df[status_col+'_end']
-del consolidated_df[ts_col+'_end']
-
-print consolidated_df.to_csv(index=False, float_format='%.0f')
+if __name__ == "__main__":
+    main()
--- example/src/ws/base_report/fs/opt/dockeroo/plot_csv.py
+++ example/src/ws/base_report/fs/opt/dockeroo/plot_csv.py
@@ -6,96 +6,106 @@
 # apt-get install python-dev build-essential libfreetype6-dev libpng12-dev pkg-config
 # pip install numpy pandas matplotlib
 
-import pandas as pd
+import argparse
 import matplotlib.pylab as plt
 import matplotlib
-import argparse
+import pandas as pd
+import sys
 
 TYPES = ['scatter', 'line', 'histogram']
 choice_types = TYPES[:]
 choice_types.append('all')
 
-# read in the args
-parser = argparse.ArgumentParser(description='Generate report charts.')
-parser.add_argument('data_file', help='data csv file, with header')
-parser.add_argument('xaxis', help='x axis')
-parser.add_argument('yaxis', help='y axis')
-parser.add_argument('filters', help='filters ie. column=value', nargs='*')
-parser.add_argument('--name', type=str, help='alternative graph name')
-parser.add_argument('--xsize', type=int, default=10, help='graph X size')
-parser.add_argument('--ysize', type=int, default=5, help='graph Y size')
-parser.add_argument('--colour', type=str, default='#000066', help='graph colour')
-parser.add_argument('--norm_x', type=bool, default=True, help='normalize x axis')
-parser.add_argument('--marker', type=str, default='.', choices=[',', '+', '.', 'o', '*'])
-parser.add_argument('--markersize', type=float, default=5.0, help='scatter plot marker size')
-parser.add_argument('--linestyle', type=str, default='-', help='line plot line style')
-parser.add_argument('--type', default='all', choices=choice_types)
-args = parser.parse_args()
+def plot(df, name, xaxis, yaxis, xsize=10, ysize=5, norm_x=True, colour='#000066', marker='.', markersize=5.0, linestyle='-', types=choice_types, **filters):
+    # normalize x axis, if required
+    if norm_x:
+        df[xaxis] = df[xaxis] - min(df[xaxis])
 
-if not args.name:
-  args.name = args.data_file
+    # apply filters
+    for c, v in filters.iteritems():
+        df = df[df[c] == v]
 
-filters = [f.split('=') for f in args.filters]
+    # cannot print empty data sets
+    if not len(df):
+        print "...no data available for %s..." % name
 
-# choose plot type, or all
-if args.type == 'all':
-    types = TYPES
-else:
-    types = [args.type]
+    # start plotting
+    matplotlib.rc('font', size=8)
+    matplotlib.rcParams['legend.fontsize'] = 8
+    matplotlib.rcParams['figure.figsize'] = xsize, ysize
+    for t in types:
+        print 'Generating %s...' % t
+        if t == 'histogram':
+            plt.hist(df[yaxis].values, color=colour)
+            plt.xlabel(xaxis)
+            plt.ylabel(yaxis)
+            plt.tight_layout()
+            plt.savefig(name+'.histogram.png', transparent=True)
+            plt.clf()    
+        else:
+            if t == 'scatter':
+                curr_marker = marker
+                curr_linestyle = ''
+                chart_file = name+'.scatter.png'
+            else:
+                curr_marker = ''
+                curr_linestyle = linestyle
+                chart_file = name+'.line.png'
+            plt.plot(df[xaxis], df[yaxis], color=colour, markeredgecolor=colour, marker=curr_marker, linestyle=curr_linestyle, markersize=markersize)
+            plt.xlabel(xaxis)
+            plt.ylabel(yaxis)
+            plt.tight_layout()
+            plt.savefig(chart_file, transparent=True)
+            plt.clf()
 
-# read in the data
-df = pd.read_csv(args.data_file, na_values=['na'])
+def main():
+    parser = argparse.ArgumentParser(description='Generate report charts.')
+    parser.add_argument('data_file', help='data csv file, with header')
+    parser.add_argument('xaxis', help='x axis')
+    parser.add_argument('yaxis', help='y axis')
+    parser.add_argument('filters', help='filters ie. column=value', nargs='*')
+    parser.add_argument('--name', type=str, help='alternative graph name')
+    parser.add_argument('--xsize', type=int, default=10, help='graph X size')
+    parser.add_argument('--ysize', type=int, default=5, help='graph Y size')
+    parser.add_argument('--norm_x', type=bool, default=True, help='normalize x axis')
+    parser.add_argument('--colour', type=str, default='#000066', help='graph colour')
+    parser.add_argument('--marker', type=str, default='.', choices=[',', '+', '.', 'o', '*'])
+    parser.add_argument('--markersize', type=float, default=5.0, help='scatter plot marker size')
+    parser.add_argument('--linestyle', type=str, default='-', help='line plot line style')
+    parser.add_argument('--type', default='all', choices=choice_types)
+    args = parser.parse_args()
 
-# validate axis exist in csv
-if args.xaxis not in df:
-    print 'Error: X axis %s is not a valid CSV column!' % args.xaxis 
-    exit(1)
-if args.yaxis not in df:
-    print 'Error: Y axis %s is not a valid CSV column!' % args.yaxis 
-    exit(1)
-for [c, v] in filters:
-    if c not in df.columns:
-        print 'Filter column %s not in file %s!' % (c, args.data_file)
+    if not args.name:
+      args.name = args.data_file
+
+    filters = [f.split('=') for f in args.filters]
+    filters = { fs[0]: fs[1] for fs in filters }
+    
+    print filters
+
+    # choose plot type, or all
+    if args.type == 'all':
+        types = TYPES
+    else:
+        types = [args.type]
+
+    # read in the data
+    df = pd.read_csv(args.data_file, na_values=['na'])
+
+    # validate axis exist in csv
+    if args.xaxis not in df:
+        print 'Error: X axis %s is not a valid CSV column!' % args.xaxis 
+        exit(1)
+    if args.yaxis not in df:
+        print 'Error: Y axis %s is not a valid CSV column!' % args.yaxis 
         exit(1)
 
-# normalize x axis, if required
-if args.norm_x:
-    df[args.xaxis] = df[args.xaxis] - min(df[args.xaxis])
+    for c, v in filters.iteritems():
+        if c not in df.columns:
+            print 'Filter column %s not in file %s!' % (c, args.data_file)
+            exit(1)
+    
+    plot(df, args.name, args.xaxis, args.yaxis, args.xsize, args.ysize, args.norm_x, args.colour, args.marker, args.markersize, args.linestyle, types, **filters)
 
-# apply filters
-for [c, v] in filters:
-    df = df[df[c] == v]
-
-# cannot print empty data sets
-if not len(df):
-    print "...no data available..."
-    exit(2)
-
-# start plotting
-matplotlib.rc('font', size=8)
-matplotlib.rcParams['legend.fontsize'] = 8
-matplotlib.rcParams['figure.figsize'] = args.xsize, args.ysize
-for t in types:
-    print 'Generating %s...' % t
-    if t == 'histogram':
-        plt.hist(df[args.yaxis].values, color=args.colour)
-        plt.xlabel(args.xaxis)
-        plt.ylabel(args.yaxis)
-        plt.tight_layout()
-        plt.savefig(args.name+'.histogram.png', transparent=True)
-        plt.clf()    
-    else:
-        if t == 'scatter':
-            marker = args.marker
-            linestyle = ''
-            chart_file = args.name+'.scatter.png'
-        else:
-            marker = ''
-            linestyle = args.linestyle
-            chart_file = args.name+'.line.png'
-        plt.plot(df[args.xaxis], df[args.yaxis], color=args.colour, markeredgecolor=args.colour, marker=marker, linestyle=linestyle, markersize=args.markersize)
-        plt.xlabel(args.xaxis)
-        plt.ylabel(args.yaxis)
-        plt.tight_layout()
-        plt.savefig(chart_file, transparent=True)
-        plt.clf()
+if __name__ == "__main__":
+    main()    

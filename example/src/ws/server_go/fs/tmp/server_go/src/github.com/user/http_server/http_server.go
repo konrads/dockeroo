ed2f687f66bbfaa4f7b1013b277af144d74654f6
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2015-11-06 15:23:49 +0000

    adding go http server

---
+++ example/src/ws/server_go/fs/tmp/server_go/src/github.com/user/http_server/http_server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "io"
+    "net/http"
+)
+
+func handle_req(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "ok")
+}
+
+func main() {
+    http.HandleFunc("/", handle_req)
+    http.ListenAndServe(":8888", nil)
+}
